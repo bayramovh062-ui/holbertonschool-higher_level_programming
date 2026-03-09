792b210678819f7c486ee993f656d02f531d5713
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-09 17:03:46 +0000

    fixed infinitive.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import sys
-count = len(sys.argv)
-sum = 0
-for i in range (1, count):
-    sum = sum + int(sys.argv[i])
-print(sum)
+if __name__ == "__main__":
+
+    count = len(sys.argv)
+    sum = 0
+    for i in range (1, count):
+        sum = sum + int(sys.argv[i])
+    print(sum)
