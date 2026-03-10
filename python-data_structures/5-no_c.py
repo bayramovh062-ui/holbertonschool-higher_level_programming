435dd053503fb96a4c3a37aa8a7ad2a2de852152
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-10 19:01:07 +0000

    added no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_string = ""
     length = len(my_string)
-    for i in range (0, length):
+    for i in range(0, length):
         if my_string[i] != "c" and my_string[i] != "C":
             new_string += my_string[i]
     return new_string
