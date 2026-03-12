28f186c5762d829429d1f42152d3df7b1fcc3b6c
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-12 18:33:08 +0000

    added dommon_elements.py

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    new_set = ()
+    for i in set_1
+        is_have = False
+        for j in set_2:
+            if i == j:
+                is_have = True
+        if is_have == True:
+            new_set.add(i)
+    return new_list
