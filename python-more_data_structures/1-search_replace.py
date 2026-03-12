aaa7b0f86b80f6156a1cb20f4477d3f6366eac17
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-12 18:15:41 +0000

    fixded replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list.replace(search, replace)
-    return my_list 
+    new_list = []
+    for i in range(0, len(my_list)):
+        if my_list[i] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[i])
+    return new_list
