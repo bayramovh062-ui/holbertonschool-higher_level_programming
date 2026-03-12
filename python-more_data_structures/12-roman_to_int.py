c811457df7bd51b4b2692323d9e546f4814d8cae
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-12 20:55:57 +0000

    added roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if  isinstance(roman_string, str):
+    if isinstance(roman_string, str):
         value_object = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         result = 0
         for i in range(0, len(roman_string)):
             if i != len(roman_string) - 1:
-                if value_object[roman_string[i]] >= value_object[roman_string[i + 1]]:
-                    result += value_object[roman_string[i]]
+                curr_value = value_object[roman_string[i]]
+                next_value value_object[roman_string[i + 1]]
+                if curr_value >= next_value:
+                    result += curr_value
                 else:
-                    result = result + (value_object[roman_string[i + 1]] - value_object[roman_string[i]])
-                    result -= value_object[roman_string[i + 1]]
+                    result = result + (next_value - curr_value)
+                    result -= next_value
             else:
-                result += value_object[roman_string[i]]
+                result += curr_value
         return result
     else:
         return 0
