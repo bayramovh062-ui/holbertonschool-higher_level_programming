c08d1f197d33cde938eeadcb342b70ae74d5803d
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-12 18:53:50 +0000

    added different_element_file

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new_set = set()
-    for i in set_1:
-        is_have = False
-        for j in set_2:
-            if i == j:
-                is_have = True
-        if not is_have:
-            new_set.add(i)
+
+    def full_set(set_1, set_2):
+        for i in set_1:
+            is_have = False
+            for j in set_2:
+                if i == j:
+                    is_have = True
+            if not is_have:
+                if i not in new_set:
+                    new_set.add(i)
+    full_set(set_1, set_2)
+    full_set(set_2, set_1)
     return new_set
