b18b3621c178bb91f712ff80cff979e998909f66
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-12 20:58:48 +0000

    added roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,9 @@ def roman_to_int(roman_string):
         value_object = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         result = 0
         for i in range(0, len(roman_string)):
+            curr_value = value_object[roman_string[i]]
+            next_value = value_object[roman_string[i + 1]]
             if i != len(roman_string) - 1:
-                curr_value = value_object[roman_string[i]]
-                next_value = value_object[roman_string[i + 1]]
                 if curr_value >= next_value:
                     result += curr_value
                 else:
