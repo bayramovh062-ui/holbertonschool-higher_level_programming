1a8f8e949b28f03095edd0cb6a0dcd9044fa3eb2
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-19 19:08:29 +0000

    added 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    Square class represents a geometric square shape.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size: The size of the square (no type/value verification yet).
+        """
+        self.__size = size
