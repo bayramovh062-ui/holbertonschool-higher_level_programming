c4accd7a9c05d118f43bbbb780b4646beebb1f06
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-21 16:40:48 +0000

    added 2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this module checks that same class or not
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    this function checks that obj in the class or not
+    """
+    return isinstance(obj, a_class)
