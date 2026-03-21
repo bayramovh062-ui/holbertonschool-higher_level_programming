675fa2093ad211d53749ad838f6279aa99ca1fc2
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-21 10:49:48 +0000

    added 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
         we can initalization object of this class
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -30,7 +30,7 @@ class Rectangle:
         """
         this function sets a value to width variable
         """
-        if value is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -50,9 +50,9 @@ class Rectangle:
         this function sets a value to height variable
         and ckecks value
         """
-        if value is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
 
-        self.__height = height
+        self.__height = value
