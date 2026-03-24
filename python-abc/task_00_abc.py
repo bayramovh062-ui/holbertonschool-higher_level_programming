5b2f181a6cc30c0e233aa72dc32703b5b5eed56d
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-24 09:55:05 +0000

    added python-abc directory, readme.md and task-00.py

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,18 +1,52 @@
 #!/usr/bin/python3
-from abc import ABC, abstactmethod
+"""
+this module explains abstract
+"""
+
+
+from abc import ABC, abstractmethod
+
 
 class Animal(ABC):
+    """
+    this class represents animals
+    """
 
     @abstractmethod
     def sound(self):
+        """
+        this is an abstractmethod that we should write
+        sound function in each subclasses for this
+        """
+
         pass
 
+
 class Dog(Animal):
+    """
+    that is a class that represetns dog
+    and this class inheritance from Animal
+    """
 
     def sound(self):
+        """
+        this is a sound function that returns
+        Bark message
+        """
+
         return "Bark"
 
+
 class Cat(Animal):
+    """
+    that is a class that represents dog
+    and this class inheritance from animal
+    """
 
     def sound(self):
+        """
+        this is a sound function that returns
+        Meow message
+        """
+
         return "Meow"
