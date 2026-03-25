c225aba48d23d961ff8339cc3b5553abb0c116d1
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-25 19:27:36 +0000

    added task01

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,62 +1,59 @@
 #!/usr/bin/env python3
 """
-Shapes, Interfaces, and Duck Typing
+Shapes, Interfaces, and Duck Typing module.
 """
-
+import math 
 from abc import ABC, abstractmethod
-import math
 
 
 class Shape(ABC):
-    """Abstract base class for shapes"""
+    """Shape abstract class."""
 
     @abstractmethod
     def area(self):
-        """Calculate area"""
+        """Returns the area of the shape."""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Calculate perimeter"""
+        """Returns the perimeter of the shape."""
         pass
 
 
 class Circle(Shape):
-    """Circle class representing a circle"""
+    """Circle class."""
 
     def __init__(self, radius):
-        """Initialize Circle with a radius"""
+        """Initializes a Circle."""
         self.radius = radius
 
     def area(self):
-        """Return the area of the circle"""
+        """Calculates Circle area."""
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        """Return the perimeter (circumference) of the circle"""
+        """Calculates Circle perimeter."""
         return 2 * math.pi * abs(self.radius)
 
 
 class Rectangle(Shape):
-    """Rectangle class representing a rectangle"""
+    """Rectangle class."""
 
-    ef __init__(self, width, height):
-        """Initialize Rectangle with width and height"""
+    def __init__(self, width, height):
+        """Initializes a Rectangle."""
         self.width = width
         self.height = height
 
     def area(self):
-        """Return the area of the rectangle"""
+        """Calculates Rectangle area."""
         return self.width * self.height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle"""
+        """Calculates Rectangle perimeter."""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """
-    Print area and perimeter using duck typing
-    """
-    print("Area: {}".format(shape.area()))
-    print("Perimeter: {}".format(shape.perimeter()))
+    """Prints the area and perimeter of a shape."""
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
