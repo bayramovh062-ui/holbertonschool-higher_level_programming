b83777fb137011737363d9e93815f2bbd0ac041f
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-25 19:38:41 +0000

    added 9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this module contains a class and this class create
+new student object and returns json format of object
+"""
+
+
+class student():
+    """
+    this class represents student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        this function will intialization values when user create a new object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    @proprty
+    def to_json(self):
+        """
+        this function retrieves a dictionary represents student
+        """
+        return self.__dict__
