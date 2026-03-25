96cf60c3fb6d8b9568d13c2afc34c1e7b4236f68
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-25 16:05:48 +0000

    added 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+"""
+this module explains write files with using function
+"""
+
+
+def write_file(file_name="", text=""):
+    """
+    this function write text to file and return length of text
+    """
+    with open(filename, 'w', encoding = "utf-8") as f:
+        f.write(text)
+    return len(text)
+
