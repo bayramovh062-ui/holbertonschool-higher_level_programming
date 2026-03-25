ff4568460a6ec36a0a837a549f1b708f87d645ac
bayramovh062-ui <bayramovh062@gmail.com>
2026-03-25 16:18:46 +0000

    added 2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhton3
+"""
+this module explains how to add a text to a file
+using pythons's write command with a mode
+"""
+
+
+def append_file(filename="", text=""):
+    """
+    this functipn append text to a file and
+    returns number of characthers added
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
