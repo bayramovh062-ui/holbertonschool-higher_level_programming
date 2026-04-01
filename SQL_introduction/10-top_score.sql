40111c8a6b275b819567ecb0a7a12c2072708915
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-02 00:35:04 +0400

    added 10-top_score.sql

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- we cacn see id and name columns order by score desc with using this code
-SELECT score, name FROM second_table;
-OREDER BY score DESC;
+SELECT score, name FROM second_table
+OREDER BY 1 DESC;
