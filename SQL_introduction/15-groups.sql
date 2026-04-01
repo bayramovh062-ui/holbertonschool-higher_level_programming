86be30374fc8d21c608167b8aae549e149ae3b55
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-02 01:09:58 +0400

    fixded 15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- we can see how much same score in the table with using this sql code
-SELECT score, count(score) FROM second_table
+SELECT score, count(score) AS number FROM second_table
 GROUP BY score
 ORDER BY score DESC;
 
