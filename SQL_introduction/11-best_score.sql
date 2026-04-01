67738bd6af60598ceec5f0e86b98a160e0bf7486
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-02 00:45:02 +0400

    added 11-best_score.sql

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- this code prints score and name cloumns wrder by score and where score >= 10
 SELECT score, name FROM second_table
-ORDER BY score DESC
-WHERE score >= 10;
+WHERE score >= 10
+ORDER BY score DESC;
