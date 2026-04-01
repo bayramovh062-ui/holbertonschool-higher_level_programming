5e543ad140e143bf7d4ec5120a2b4a070e986f4a
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-02 01:15:49 +0400

    added 16-no_link.sql

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- we can see score and name coumns and all rows with using this code but where name is not null
 SELECT score, name FROM second_table
-WHERE score IS NOT NULL
+WHERE name IS NOT NULL
 ORDER BY score DESC;
