c54c911d422b2558f90fc8a42cf78eb49bd4691e
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 01:12:58 +0400

    added 1-create_user.sql

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- we creates a new user in this code and if the user exists code should'nt fail then we give all permissions to user
+
+CREATE USER 'user_0d_1'@'localhost' BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON .*. TO 'user_0d_1'@'localhost';
