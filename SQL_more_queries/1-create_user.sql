9c0aa87298471a8a0654242a4d1fcdd5d1d87761
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 01:23:11 +0400

    fixed 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- we creates a new user in this code and if the user exists code should'nt fail then we give all permissions to user
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENDIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
