032e7667212b3a5713c2321079a6e68eb869a45e
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 00:41:46 +0400

    added SQL_more_queries and README.md, task0 privelege task

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- this sql code shows user1 and user2 's permissions on database
+SHOW GRANTS FOR 'user_0d_1@localhost';
+SHOW GRANTS FOR 'user_0d_2@localhost';
---
+++ SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+# this directory contains sql codes and this codes explain how to use grant, subqueries, join and etc
