b5c106e47289d93e7fbd68e0f09a26dc2cc3b8f6
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 20:33:49 +0400

    added 9-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- this code gives us an examle about subquery
 
-SELECT id, name FROM cities
-WHERE id = (
-	SELECT state_id FROM states
+SELECT state_id, name FROM cities
+WHERE state_id = (
+	SELECT id FROM states
 	WHERE name = 'california'
 )
 ORDER BY id;
