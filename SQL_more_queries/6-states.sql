21f4367c2f9db443c8301c33300c40c86813b804
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 13:44:05 +0400

    added 6-states.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- this code creates new database and table in this table id should be primary key and auto_generated and name not null
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT PRIMARY KEY AUTO_GENERATED, name VARCHAR(256) NOT NULL);
