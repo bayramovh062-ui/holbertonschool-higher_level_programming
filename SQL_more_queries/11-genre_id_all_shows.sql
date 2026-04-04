8da18818f6f152a1468967e5cdd1a7be5b6b43e3
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 21:33:18 +0400

    added 11-genre_id_all_shows.sql

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- we can list tw_shows base's title column and tv_show_genres base's genre_id column with using this code
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_shows_genres.genre_id;
