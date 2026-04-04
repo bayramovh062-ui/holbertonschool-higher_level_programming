85d949de6bb1953ebb0b7cc30ee3559afeb43181
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 21:06:06 +0400

    added 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 JOIN tv_show_genres ON tv_shows_id = tv_show_genres.show_id
-ORDER BY tv_shows_title, th_shows_genres.genre_id
+ORDER BY tv_shows_title, th_shows_genres.genre_id;
