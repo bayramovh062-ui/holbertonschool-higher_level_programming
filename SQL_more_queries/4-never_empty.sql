84fa9957a88501ea16be07ab3f4b60155a0e7ca8
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 13:31:49 +0400

    fixed never_empty.sql

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- we can create new table and gives default value to id column in this table with using this code
 
-CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1,name VARCHAR(256);
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256);
