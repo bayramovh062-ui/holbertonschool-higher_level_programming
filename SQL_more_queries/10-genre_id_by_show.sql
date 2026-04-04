a06205ce1a1d8c648e9ab5c854c9d4fbf3dca941
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 21:09:45 +0400

    fixed 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- this code gives us an example about inner join
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_shows_id = tv_show_genres.show_id
-ORDER BY tv_shows_title, tv_shows_genres.genre_id;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
