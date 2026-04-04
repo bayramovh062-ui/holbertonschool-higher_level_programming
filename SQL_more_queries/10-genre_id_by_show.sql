e1f89c750bcaee554fddd3e413f48727b11a8eee
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 21:00:17 +0400

    added task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- this code gives us an example about inner join
 
-SELECT tv_shows.tittle, tv_show_genres.genre_id from tv_shows
+SELECT tv_shows.tittle, tv_show_genres.genre_id FROM tv_shows
 JOIN tv_show_genres ON tv_shows_id = tv_show_genres.show_id;
