02e225bcc778d2b27e0ad28575dadd32ffa686c4
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-04 13:59:05 +0400

    added 7-cities.sql

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- this code creates a new database and table and gives constraints in this table id state id name columns
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT PRIMARY KEY AUTO_INCREMENT, state_id INT, FOREIGN KEY(state_id) REFERENCES states(state_id, name VARCHAR(256) NOT NULL)
