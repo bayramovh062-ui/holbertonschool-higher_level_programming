87dfda6ef7535f155ee62b99ca32cfd11ffb8009
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-09 23:56:55 +0400

    fixed task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     c.execute(
                 "SELECT * FROM states WHERE name LIKE BINARY '{}'"
                 "ORDER BY id"
-            ).format(state_name)
+            .format(state_name))
     rows = c.fetchall()
     for row in rows:
         print(row)
