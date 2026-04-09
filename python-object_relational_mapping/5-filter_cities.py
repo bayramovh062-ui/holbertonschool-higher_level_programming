4f71fc6efd06e0507bece8d2c5e92128ae44e51b
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-10 00:39:07 +0400

    added 5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+this code shows cities and states table's data
+with using join
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username, password, name, state_name = sys.argv[1:]
+    db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=name,
+            port=3306
+    )
+    c = db.cursor()
+    c.execute(
+            "SELECT cities.id, cities.name, states.name "
+            "FROM cities JOIN states ON cities.state_id = states.id "
+            "WHERE states.name = %s "
+            "ORDER BY cities.id"
+        ), (state_name,)
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
