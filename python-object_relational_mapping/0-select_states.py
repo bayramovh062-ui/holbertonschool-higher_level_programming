567a019bcccaecf16dc50651bf536f08f1b347d1
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-09 22:42:39 +0400

    fixed task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,15 +4,21 @@ this python code shows us state table's data
 import sys
 import MySQLdb
 
-
-usernamedb = sys.argv[1]
-passworddb = sys.argv[2]
-namedb = sys.argv[3]
-db = MySQLdb.connect(
-        host = "localhost",
-        username = usernamedb,
-        password = passworddb,
-        database = namedb
-)
-c = db.cursor()
-print(c.execute("SELECT states FROM hbtn_0e_0_usa ORDER BY id"))
+if __name__ = "__main__":
+    usernamedb = sys.argv[1]
+    passworddb = sys.argv[2]
+    namedb = sys.argv[3]
+    db = MySQLdb.connect(
+            host = "localhost",
+            user = usernamedb,
+            passwd = passworddb,
+            db = namedb
+            port = 3306
+    )
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY id")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
