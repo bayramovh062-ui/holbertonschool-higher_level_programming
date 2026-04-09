6f293bd2d7cf86652fa43497fd2d15cdbb3d904b
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-10 00:26:26 +0400

    added 4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+this code shows cities and states table's data
+with using join
+"""
+
+
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    username, password, name = sys.argv[1:]
+    db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=name,
+            port=3306
+    )
+    c = db.cursor()
+    rows = c.execute(
+                "SELECT cities.id, cities.name, states.name",
+                "FROM cities JOIN states ON cities.state_id"
+                "ORDER BY cities.id"
+            )
+    for row in rowsL:
+        print(row)
+    c.close()
+    db.close()
