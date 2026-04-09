6e2a5d581d96ca48764b53efa4953709bc4f62cd
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-09 22:52:26 +0400

    fixed task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     passworddb = sys.argv[2]
     namedb = sys.argv[3]
     db = MySQLdb.connect(
-            host = "localhost",
-            user = usernamedb,
-            passwd = passworddb,
-            db = namedb,
-            port = 3306
+      host="localhost",
+      user=usernamedb,
+      passwd=passworddb,
+      db=namedb,
+      port=3306
     )
     c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY id")
