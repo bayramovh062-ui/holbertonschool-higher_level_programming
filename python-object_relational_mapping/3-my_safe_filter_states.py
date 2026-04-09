3b2f41a582b7339f4a682d62ffaa05e5394b3a4c
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-10 00:04:02 +0400

    added 3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+this code shows us state table's data but filtered
+version with user's input and we will use this input in
+the code for find our state name should starting which symbol
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username, password, name, state_name = sys.argv[1:]
+    db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=name,
+            port=3306
+        )
+    c = db.cursor()
+    c.execute(
+                "SELECT * FROM states WHERE name LIKE BINARY %s"
+                "ORDER BY id", (state_name,)
+            )
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
