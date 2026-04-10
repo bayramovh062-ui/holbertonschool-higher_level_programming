83b033b5ffa28a63e23f47546973623c47d60b0c
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-11 01:25:00 +0400

    fixed 10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -12,7 +12,10 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    username, password, db_name, state_name = sys.argv[1:]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
     engine = create_engine(
                 'mysql+mysqldb://{}:{}@localhost:3306/{}'/format(
                         username, password, db_name
