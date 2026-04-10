b0a7b48b6c17c9c3979960c3225123cebbcb2231
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-11 01:16:59 +0400

    added 10-model_state_my_get.py

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+this code shows us a row from state table
+and this row's state name should be
+user's input otherwise print Not found
+"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username, password, db_name, state_name = sys.argv[1:]
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'/format(
+                        username, password, db_name
+                    ),
+                pool_pre_ping=True
+            )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = (
+                session.query(State)
+                .filter(State.name == state_name)
+                .order_by(State.id)
+                .first()
+            )
+    if state is None:
+        print("Not found")
+    else:
+        print("{}".format(state.id))
+    session.close()
