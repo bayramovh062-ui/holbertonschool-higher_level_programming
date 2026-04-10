4fe8505589896a0aeb43f1deaf7fb0cf3f906b84
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-11 01:45:49 +0400

    added task10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -12,13 +12,9 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4].strip("'")
     engine = create_engine(
                 'mysql+mysqldb://{}:{}@localhost:3306/{}'/format(
-                        username, password, db_name
+                        sys.argv[1], sys.argv[2], sys.argv[3]
                     ),
                 pool_pre_ping=True
             )
@@ -26,7 +22,7 @@ if __name__ == "__main__":
     session = Session()
     state = (
                 session.query(State)
-                .filter(State.name == state_name)
+                .filter(State.name == sys.qrgv[4])
                 .first()
             )
     if state is None:
