b55e1c80de8e83ace2cd306bfb195e6907d0f5ce
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-10 21:57:12 +0400

    added model_state.py

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+this code creates new table with name and id column
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    this class represents state table
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False )
