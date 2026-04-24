bdaf7fed25b5de18c6e2bc4c3d2a78082c8fbcb9
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-24 23:32:17 +0400

    added 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ const argvArr = process.argv.slice(2);
 if (argvArr.length <= 1) {
   console.log(0);
 } else {
-  argvArr.splice(argvArr.indexOf(Math.max(argvArr)), 1);
+  argvArr.splice(argvArr.indexOf(Math.max(...argvArr)), 1);
   console.log(Math.max(...argvArr));
 }
