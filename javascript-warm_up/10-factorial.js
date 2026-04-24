be3e613032d0a86e2705107d52e162a4de614804
bayramovh062-ui <bayramovh062@gmail.com>
2026-04-24 23:13:08 +0400

    added 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 function factorial (n) {
-  let result = 1;
   if (isNaN(n) || n === 1 || n === 0) {
     return 1;
   } else {
-    result *= n;
-    factorial(n - 1);
+    return n * factorial(n - 1);
   }
-  return result;
 }
 
 console.log(factorial(parseInt(process.argv[2], 10)));
